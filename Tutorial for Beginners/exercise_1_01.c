90756363c564c66345d9fad5c3154191db2c6413
bmuraya <babumuraya@gmail.com>
2023-03-12 13:06:49 +0300

    hello world

--- Tutorial for Beginners/exercise_1_01.c
+++ Tutorial for Beginners/exercise_1_01.c
@@ -1,13 +1,39 @@
-#include <stdio.h>
+// Documentation section
+// single line comments
+/*  Authors  babu muraya
+    Date 12/03/2023
+    hello world progam print
+**/
+#include <stdio.h> //  (link section) built in function and standard library for printf scanf
 
- main()
+
+ // definition section
+ #define MAX 100  /*EVERY  WHERE MAX IT WILL BE REPLCE BY 100 VALUE SO RATHER
+ THAN WRITING 100 EVERY TIME  YOU CAN USE JUST MAX EVERY TIME YOU USE MAX */
+
+ /*GLOBAL DECLARARION SECTIION    FUNTION THAT ARE USE IN MORE THAT ONE
+ CAN BE DECLARED OUTSIDE THE FUNCTION */ 
+
+// Global declaration of a variable   int a,
+// Global declaration of a function eg void sum();
+//                                     void sub();
+
+
+// main section is a must any program you have to have one main function 
+
+ main() // function
 {
-    printf("Hello, World\n");
-    printf("Hello, World new line \t");
-    printf("Hello, World third line \b");
-    printf("Hello, World third line \\");
+    // Declaration and executionsection
+    printf("Hello, World\n"); // new line
+    printf("Hello, World new line \t");  // tab
+    printf("Hello, World third line \b"); // backlash
+    printf("Hello, World third line \\"); // double slash
     //printf("Hello, World third line \");
-   // printf("Hello, World third line \c");
+   // printf("Hello, World third line \c"); comments
 
     return (0);
 }
+
+//sub program section Optional 
+
+
