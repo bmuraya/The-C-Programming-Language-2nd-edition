3919ddfa666af2faf13c1246d61d10c5a8307b17
bmuraya <babumuraya@gmail.com>
2023-03-21 20:59:11 +0300

    #include <stdio.h>

---
+++ Chapter_1/exercise_1_6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* verify that the expression get chs() != EOF is ) or 1 */
+
+int main()
+{
+  printf("value of expression: %d", getchar() != EOF);
+
+  return 0;
+
+}
+
+
+
---
+++ Chapter_1/exercise_1_7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* verify that the expression get chs() != EOF is ) or 1 */
+
+int main()
+{
+  printf("EOF: %d", EOF);
+
+  return 0;
+
+}
+
