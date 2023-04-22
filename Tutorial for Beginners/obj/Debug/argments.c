bad51bd1a82bbb7ab261c042885fd1b92712f95b
bmuraya <babumuraya@gmail.com>
2023-04-22 23:40:55 +0300

    trail

--- Tutorial for Beginners/obj/Debug/argments.c
+++ Tutorial for Beginners/obj/Debug/argments.c
@@ -15,7 +15,7 @@ int main(int __attribute__((unused)) ac, char **av)
 /*int main(int ac, char **av) {
     printf("Program name: %s\n", av[0]);
     printf("Number of arguments: %d\n", ac - 1);
-    for (int i = 1; i < ac; i++) {
+    for (int i = 1; i < ac; i++) {git
         printf("Argument %d: %s\n", i, av[i]);
     }
     return 0;
